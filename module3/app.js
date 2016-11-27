026312a010d96c09263308934af11e2a2ba975b9
Kelly Lougheed <kellylougheed@gmail.com>
2016-11-27 13:53:59 -0800

    Upload module 3

---
+++ module3/app.js
@@ -0,0 +1,72 @@
+(function() {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemsDirective);
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'list.html',
+      scope: {
+        items: '<',
+        onRemove: '&'
+      },
+    };
+    return ddo;
+  };
+
+  NarrowItDownController.$inject = ['MenuSearchService', '$http'];
+  function NarrowItDownController(MenuSearchService, $http) {
+    var controller = this;
+
+    controller.searchTerm = MenuSearchService.getSearchTerm();
+
+    controller.foundItems = MenuSearchService.getMatchedMenuItems();
+
+    controller.search = function(searchTerm) {
+      MenuSearchService.getMatchedMenuItems(searchTerm);
+    };
+
+    controller.removeItem = function(itemIndex) {
+      MenuSearchService.removeItem(itemIndex);
+    }
+
+  }
+
+  function MenuSearchService($http) {
+    var service = this;
+
+    var searchTerm = "";
+
+    service.getSearchTerm = function() {
+      return searchTerm;
+    };
+
+    service.removeItem = function(itemIndex) {
+      foundItems.splice(itemIndex, 1);
+    };
+
+    service.getMatchedMenuItems = function(searchTerm) {
+      return $http({
+        method: "GET",
+        url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+        }).then(function (result) {
+          var foundItems = [];
+          var menu = result.data.menu_items;
+          for (var i = 0; i < result.data.menu_items.length; i++) {
+            if (menu[i].description.includes(searchTerm) || menu[i].name.includes(searchTerm)) {
+              foundItems.push(menu[i]);
+            }
+          }
+          return foundItems;
+      });
+    };
+
+    
+  }
+
+
+})();
